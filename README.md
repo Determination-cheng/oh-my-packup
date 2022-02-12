a6e0d18f62baaf0af4a6660636f7d9e06c388c75
Cheng <1003306162@qq.com>
2022-02-12 16:16:11 +0800

    init 初始化打包目标

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # oh-my-packup
-packup core
+打包器核心原理
+
+
+
+## 目标
+
+**依赖**
+
+index.html -> main.js -> bar.js
+
+**目标**
+
+将所有 js 文件打包成一个文件，并成功运行
---
+++ example/main.js
@@ -0,0 +1,10 @@
+import { bar } from './bar';
+
+console.log('main')
+
+export function main() {
+    console.log('main function')
+}
+
+bar()
+main()
