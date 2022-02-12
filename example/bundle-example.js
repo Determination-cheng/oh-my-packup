128cb1b437040e1b682b20b93f3345fa67bef253
Cheng <1003306162@qq.com>
2022-02-12 19:19:17 +0800

    feat 完善打包文件范式

--- example/bundle-example.js
+++ example/bundle-example.js
@@ -1,47 +1,43 @@
-function mainjs(require, module, exports) {
-    //* import 只能用在文件的顶层作用域，否则会报错
-    //* 因此此处需要转成 commonJS 格式
-    // import {bar} from './bar.js';
-    const {bar} = require('./bar.js')
+;(function (modules) {
+    //* 1.实现 require()
+    function require(filePath) {
+        const fn = modules[filePath]
 
-    console.log('main')
+        const module = {
+            exports: {}
+        }
 
-    function main() {
-        console.log('main function')
+        fn(require, module, module.exports)
+        return module.exports
     }
 
-    bar()
-    main()
+    //* 2.执行入口文件
+    require('./main.js')
+})({
+    './main.js': function (require, module, exports) {
+        //* import 只能用在文件的顶层作用域，否则会报错
+        //* 因此此处需要转成 commonJS 格式
+        // import {bar} from './bar.js';
+        const {bar} = require('./bar.js')
 
-    module.exports = {main}
-}
+        console.log('main')
 
-function barjs(require, module, exports) {
-    console.log('bar')
+        function main() {
+            console.log('main function')
+        }
 
-    function bar() {
-        console.log('bar function')
-    }
+        bar()
+        main()
 
-    module.exports = {bar}
-}
+        module.exports = {main}
+    },
+    './bar.js': function (require, module, exports) {
+        console.log('bar')
 
-//* 1.实现 require()
-function require(filePath) {
-    const map = {
-        './bar.js': barjs,
-        './main.js': mainjs,
-    }
+        function bar() {
+            console.log('bar function')
+        }
 
-    const fn = map[filePath]
-
-    const module = {
-        exports: {}
-    }
-
-    fn(require, module, module.exports)
-    return module.exports
-}
-
-//* 2.执行入口文件
-require('./main.js')
+        module.exports = {bar}
+    },
+});
