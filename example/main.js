eaced3ccb71e6eed494dd4eb247ce88e9b69f885
Cheng <1003306162@qq.com>
2022-02-12 16:57:09 +0800

    feat 生成目录依赖关系图

--- example/main.js
+++ example/main.js
@@ -1,4 +1,4 @@
-import { bar } from './bar';
+import { bar } from './bar.js';
 
 console.log('main')
 
--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 import fs from 'fs'
+import path from 'path'
 import parser from '@babel/parser'
+import traverse from '@babel/traverse'
+
+function getDependencies(filePath) {
+    //* 文件内容
+    const source = fs.readFileSync(filePath, {encoding: 'utf-8'})
+    const ast = parser.parse(source, {sourceType: 'module'})
+
+    //* 文件依赖
+    const deps = []
+    traverse.default(ast, {
+        ImportDeclaration(path) {
+            deps.push(path.node.source.value)
+        }
+    })
+
+    return {source, deps}
+}
+
+function createGraph() {
+    const root = getDependencies('./example/main.js')
+    const graph = [root]
+
+    for (const {deps} of graph) {
+        deps.forEach((dep) => {
+            const filePath = path.resolve('./example', dep)
+            const child = getDependencies(filePath)
+            graph.push(child)
+        })
+    }
+
+    return graph
+}
 
 function run() {
-    const entry = fs.readFileSync('./example/main.js', {encoding: 'utf-8'})
-    //* 获取入口文件的 AST
-    const ast = parser.parse(entry, {sourceType: 'module'})
-    console.log(ast)
-
+    createGraph()
 }
 
 run()
